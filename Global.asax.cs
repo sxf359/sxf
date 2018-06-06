7edaf7d20bb5adf27a2fe3a51cf680975038a589
sxf359 <sxf359@sohu.com>
2018-06-06 21:30:26 +0800

    rabbitmq

--- Global.asax.cs
+++ Global.asax.cs
@@ -7,17 +7,19 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using LMSoft.Web.Hubs;
 using SXF.Utils.QuartzScheduler;
-using System.Linq;
-
 namespace LMSoft.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         //create
-        private static QuartzWorker _worker;
-       
+        static QuartzWorker _worker;
+        protected void Application_PreSendRequestHeaders()
+        {
+
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -26,25 +28,42 @@ namespace LMSoft.Web
             //RouteConfig.RegisterHandler(RouteTable.Routes);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            int minWorker, minIOC;
+            // Get the current settings.
+            ThreadPool.GetMinThreads(out minWorker, out minIOC);
+            //服务器设置最小的进程中线程为240，因为是8核cpu。单核30，8核是：8*30
+            ThreadPool.SetMinThreads(30, minIOC);
+            //if (!Utility.IsLocal())
+            //{
+            //    ThreadPool.SetMinThreads(30, minIOC);
+            //}
+
+            Thread th = new Thread(() => { LMSoft.Web.Hubs.RabbitMQInstance.Listening(); });
+            th.Start();
+
+            Thread th1 = new Thread(() => { LMSoft.Web.Hubs.RabbitMQInstance.ListeningRabbitMQTest(); });
+            th1.Start();
+
             // 定时工作任务
             _worker = new QuartzWorker();
-            var recycleWork = new LMSoft.Web.Hubs.TheTask.RecycleCustomerWork();
-            _worker.AddWork(recycleWork);
-            //var testWork = new LMSoft.Web.Hubs.TheTask.TestWork();
-            //_worker.AddWork(testWork);
+            Hubs.TheTask.QuestionsLibraryAndSortListRightPushWork questionsLibraryAndSortWork = new Hubs.TheTask.QuestionsLibraryAndSortListRightPushWork();
+            _worker.AddWork(questionsLibraryAndSortWork);
+
+            Hubs.TheTask.DeleteExamWork deleteExamWork = new Hubs.TheTask.DeleteExamWork();
+            _worker.AddWork(deleteExamWork);
             _worker.Start();
 
-
         }
-        private void Session_End(object sender, EventArgs e)
+        void Session_End(object sender, EventArgs e)
         {
 
             //这里设置你的web地址，可以随便指向你的任意一个aspx页面甚至不存在的页面，目的是要激发Application_Start
             //使用您自己的URL
-            //string url = "http://localhost";
-            //System.Net.HttpWebRequest myHttpWebRequest = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
-            //System.Net.HttpWebResponse myHttpWebResponse = (System.Net.HttpWebResponse)myHttpWebRequest.GetResponse();
-            //System.IO.Stream receiveStream = myHttpWebResponse.GetResponseStream();//得到回写的字节流
+            string url = "http://localhost";
+            System.Net.HttpWebRequest myHttpWebRequest = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            System.Net.HttpWebResponse myHttpWebResponse = (System.Net.HttpWebResponse)myHttpWebRequest.GetResponse();
+            System.IO.Stream receiveStream = myHttpWebResponse.GetResponseStream();//得到回写的字节流
 
 
 
@@ -53,12 +72,10 @@ namespace LMSoft.Web
             // 注意: 只有在 Web.config 文件中的 sessionstate 模式设置为 InProc 时，才会引发 Session_End 事件。
             // 如果会话模式设置为 StateServer 或 SQLServer，则不会引发该事件。
         }
-
         protected void Application_Error(object sender, EventArgs e)
         {
 
         }
-
         protected void Application_End(object sender, EventArgs e)
         {
             //在应用程序关闭时运行的代码
@@ -67,17 +84,9 @@ namespace LMSoft.Web
                 _worker.Stop();
             }
         }
-
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            //if (Request.HttpMethod == "GET")
-            //{
-            //    //EventLog.WriteLog("a");
-            //    //Response.Clear();
-            //    Response.BufferOutput = true;
-            //    //Response.Flush();
-            //    //Response.End();
-            //}
+
         }
 
     }
